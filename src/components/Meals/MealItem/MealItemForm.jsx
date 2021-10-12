c14f2be5dfa00fbbc2f3d2ecadd16651307013e4
Daris <daris.hodzic.dh@gmail.com>
2021-10-12 18:14:26 +0200

    form

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Input from "./Input";
+import classes from "./MealItemForm.module.css";
 
-const MealItemForm = (params) => {};
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
